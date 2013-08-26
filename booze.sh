630155ff0212788432e16f0b324f9c189d5bb3d5
Zev Weiss <zev@bewilderbeest.net>
2013-08-25 17:07:11 -0700

    Add a small library of useful things for booze filesystems.
    Mostly just shell versions of various system #defines.

---
+++ booze.sh
@@ -0,0 +1,30 @@
+
+# A library of sorts providing various facilities for booze
+# filesystems.
+
+enable -f ./booze.so booze
+
+__getmacros() { gcc -E -dD -x c - <<<"#include <$1.h>"; }
+
+# Define all system errno values
+eval `__getmacros errno | sed -n -r 's/^#define (E[^ ]+) ([0-9]+)$/\1=\2/p;'`
+
+# Define access(2) mask values
+eval `__getmacros unistd | sed -n -r 's/^#define ([^ ]+_OK) ([0-9]+)$/\1=\2/p;'`
+
+# Define O_* open(2) flags
+eval `__getmacros fcntl | sed -n -r 's/^#define (O_[^ ]+) ([0-9]+)$/\1=\2/p;'`
+
+# Define S_* mode flags
+eval `__getmacros linux/stat | sed -n -r 's/^#define (S_[^ ]+) ([0-9]+)$/\1=\2/p;'`
+
+for __t in LNK REG DIR CHR BLK SOCK; do
+	eval "S_IS$__t() { [ \"\$((\$1 & \$S_IF$__t))\" -eq \"\$((S_IF$__t))\" ]; }"
+done
+
+# This one doesn't fit the S_IS*/S_IF* pattern
+S_ISFIFO() { [ "$(($1 & $S_IFIFO))" -eq "$((S_IFIFO))" ]; }
+
+# Namespace cleanup
+unset -v __t
+unset -f __getmacros
--- hello.sh
+++ hello.sh
@@ -1,16 +1,19 @@
 
-enable -f ./booze.so booze
+. ./booze.sh
 
 booze_getattr()
 {
+	local now=`date +%s`
+	local times="$now $now $now"
+	local ids="`id -u` `id -g`"
 	if [ "$1" == "/" ]; then
-		booze_out="0 0040755 2 `id -u` `id -g` 0 0 0 0 0 0"
+		booze_out="0 $(printf '%o' $((S_IFDIR | 0755))) 2 $ids 0 0 0 $times"
 		return 0
 	elif [ "$1" == "/booze" ]; then
-		booze_out="1 0100644 1 `id -u` `id -g` 0 7 1 0 0 0"
+		booze_out="1 $(printf '%o' $((S_IFREG | 0644))) 1 $ids 0 7 1 $times"
 		return 0
 	else
-		booze_err=-2
+		booze_err=-$ENOENT
 		return 1
 	fi
 }
@@ -26,7 +29,7 @@ booze_open()
 	if [ "$1" == "/booze" ]; then
 		return 0
 	else
-		booze_err=-2
+		booze_err=-$ENOENT
 		return 1
 	fi
 }
@@ -35,7 +38,7 @@ data=$'Booze!\n'
 booze_read()
 {
 	if [ "$1" != "/booze" ]; then
-		booze_err=-2
+		booze_err=-$ENOENT
 		return 1
 	fi
 
